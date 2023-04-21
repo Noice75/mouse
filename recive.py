db5ff4391e5f75313a39cf97077e4cc70768c9e7
Goku Son <abcasdfe@gmail.com>
2023-04-21 13:26:04 +0530

    UwU

--- recive.py
+++ recive.py
@@ -8,18 +8,12 @@ import threading
 
 MCAST_GRP = '224.0.0.1'
 MCAST_PORT = 5007
-IS_ALL_GROUPS = True
 
+# Socket Setup
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-if IS_ALL_GROUPS:
-    # on this port, receives ALL multicast groups
-    sock.bind(('', MCAST_PORT))
-else:
-    # on this port, listen ONLY to MCAST_GRP
-    sock.bind((MCAST_GRP, MCAST_PORT))
+sock.bind(('', MCAST_PORT))
 mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
-
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 # Defined functions
@@ -33,7 +27,7 @@ fnDir = {
 
 while True:
     data = pickle.loads(sock.recv(1024))
-    if (data[0]["fn"] >= 50):
+    if (data[0]["fn"] >= 50):  # Threaded function needs to have key > 50
         threading.Thread(
             target=fnDir[data[0]["fn"]], args=(data[0],)).start()
     else:
